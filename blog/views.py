de80f8e3644962f28e38aef35a2de8188c72a272
Milind Chauhan <milindchauhan95@gmail.com>
2022-08-09 23:47:41 +0530

    render about page using template

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Milind Chauhan',
+        'title': 'blog-post-1',
+        'content': 'this is a test blog post',
+        'created': 'august 9th 2022',
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'blog-post-2',
+        'content': 'this is a second test blog post',
+        'created': 'august 9th 2022',
+    },
+]
+
+
 # Create your views here.
 def home(request):
     return render(request, "blog/home.html")
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, "blog/about.html")
